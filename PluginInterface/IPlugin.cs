184db59a57beb557024b4cb32809233297295e2b
Belim <github@builtbybel.com>
2024-08-19 18:17:32 +0200

    Update PlugInterface

--- PluginInterface/IPlugin.cs
+++ PluginInterface/IPlugin.cs
@@ -5,5 +5,12 @@ namespace PluginInterface
     public interface IPlugin
     {
         UserControl GetControl();
+
+        // Property for Plugin information
+        string PluginName { get; }
+        string PluginVersion { get; }
+        string PluginInfo { get; }
+
+
     }
 }

b56638094cdc1fc033eb4801eb0676ec9dc9ad14
Belim <belim@builtbybel.com>
2024-08-26 17:33:35 +0200

    Add files via upload

---
+++ plugins/Restart Explorer.ps1
@@ -0,0 +1,18 @@
+﻿# Function to show a message box
+function Show-MessageBox {
+    param (
+        [string]$message,
+        [string]$title = "Information"
+    )
+    Add-Type -AssemblyName PresentationFramework
+    [System.Windows.MessageBox]::Show($message, $title)
+}
+
+# Stop Windows Explorer
+Stop-Process -Name explorer -Force
+
+# Start Windows Explorer
+Start-Process explorer
+
+# Show message box
+Show-MessageBox -message "Windows Explorer has been restarted successfully." -title "xd-AntiSpy"
